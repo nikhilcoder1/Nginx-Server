1f501d821ccf1c92094c0dfc2e5df5a858a59560
dev_nick <developernikhil28@gmail.com>
2025-10-18 11:29:40 +0530

    Add Nginx server

---
+++ server/server.js
@@ -0,0 +1,42 @@
+import { createServer } from "http";
+import { readFile } from "fs";
+import { join, extname } from "path";
+
+const port = 3000;
+
+const server = createServer((req, res) => {
+  const filePath = join(
+    __dirname,
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  const extName = extname(filePath).toLocaleLowerCase();
+
+  let mimeTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "image/png",
+  };
+
+  const contentType = mimeTypes[extName] || "application/octet-stream";
+
+  readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.end("404: File not found bro");
+      } else {
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
